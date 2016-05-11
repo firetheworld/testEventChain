e8613f0dd2b728b9bc6395b073c079c43c2d3eaa
Flame <firetheworld@hotmail.com>
2016-05-11 15:26:08 +0800

    Initial Commit

---
+++ testEventChain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testEventChain
+//
+//  Created by liangkai.zheng on 16/5/11.
+//  Copyright © 2016年 liangkai.zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testEventChain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testEventChain
+//
+//  Created by liangkai.zheng on 16/5/11.
+//  Copyright © 2016年 liangkai.zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
